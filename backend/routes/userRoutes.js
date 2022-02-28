e96c4c29db84c68210d9ff30a1fa2a322e16767d
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-02-28 15:54:51 +0530

    Update Profile Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,9 +7,13 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 
 router.post("/", registerUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;
