dc328aeb614b46e6144f2a64a2cf0b506fe251b4
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-03-01 16:01:39 +0530

    Order Controllers and Routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import addOrderItems from "../controllers/orderController.js";
+const router = express.Router();
+
+import { protect } from "../middleware/authMiddleware.js";
+
+router.route("/").post(protect, addOrderItems);
+
+export default router;
