04e6f21b315970dfe4133f287c074f63b1f978f4
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-03-01 19:12:43 +0530

    Getting OrderEndpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import addOrderItems from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 const router = express.Router();
 
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
