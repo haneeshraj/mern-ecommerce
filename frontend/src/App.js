a2d07269494ff59a5de243a3a4302c37c186afc7
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-03-04 13:40:00 +0530

    UserListScreen and User list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/' element={<HomeScreen />} />
             <Route path='/products/:id' element={<ProductScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/cart'>
               <Route path=':id' element={<CartScreen />} />
               <Route path='' element={<CartScreen />} />
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -53,3 +53,16 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const userListReducer = (state = { user: [] }, action) => {
+  switch (action.type) {
+    case "USER_LIST_REQUEST":
+      return { loading: true };
+    case "USER_LIST_SUCCESS":
+      return { loading: false, users: action.payload };
+    case "USER_LIST_FAIL":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
