d30fa2249cf7b8972639f357ec92c43926050f11
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2022-03-07 21:12:52 +0530

    Product Review

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -126,3 +126,36 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     });
   }
 };
+
+export const createProductReview =
+  (productId, review) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: "PRODUCT_CREATE_REVIEW_REQUEST" });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+      await axios.post(
+        `http://localhost:5000/api/products/${productId}/reviews`,
+        review,
+        config
+      );
+
+      dispatch({ type: "PRODUCT_CREATE_REVIEW_SUCCESS" });
+    } catch (error) {
+      dispatch({
+        type: "PRODUCT_CREATE_REVIEW_FAIL",
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.response,
+      });
+    }
+  };
